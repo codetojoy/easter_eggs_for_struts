1be088e6c3b8b4da90f484a7286b750e2a84fb59
Michael Easter <codetojoy@gmail.com>
2018-10-13 08:47:52 -0300

    new simple example

---
+++ egg3_sandbox/RunIt.md
@@ -0,0 +1,8 @@
+
+### Notes
+
+* use Tomcat 8
+    * .e.g ~/tools/tomcat_8_5_32_5151
+* `./go_build_and_deploy.sh`
+    * see link in output
+
---
+++ egg3_sandbox/src/main/java/net/codetojoy/action/EditScenarioAction.java
@@ -0,0 +1,44 @@
+package net.codetojoy.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import net.codetojoy.model.*;
+import net.codetojoy.service.*;
+
+import org.apache.log4j.Logger;
+
+public class EditScenarioAction extends ActionSupport {
+
+    private static final Logger logger = Logger.getLogger(EditScenarioAction.class);
+    private static final String LOG_PREFIX = "TRACER EditScenarioAction  ";
+
+    private void logIt(String msg) {
+        logger.error(LOG_PREFIX + " " + msg);
+    }
+
+	private static final long serialVersionUID = 1L;
+
+	private ScenarioService scenarioService;
+	private Scenario scenarioBean;
+
+	public String execute() throws Exception {
+        logIt("execute begin");
+	    scenarioService.saveScenario( getScenarioBean() );
+		return SUCCESS;
+	}
+
+	public String input() throws Exception {
+        logIt("input begin");
+		setScenarioBean( scenarioService.getScenario() );
+		return INPUT;
+	}
+
+	public Scenario getScenarioBean() { return scenarioBean; }
+	public void setScenarioBean(Scenario scenario) { scenarioBean = scenario; }
+
+	public ScenarioService getScenarioService() { return scenarioService; }
+	public void setScenarioService(ScenarioService scenarioService) {
+        logIt("setting scenario service");
+        this.scenarioService = scenarioService;
+    }
+}
